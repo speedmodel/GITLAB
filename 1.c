20345f259b6d80652ce2cd181013fb59b669e804
Thanarit <cheast-naogi@hotmail.com>
2018-03-23 18:00:03 +0700

    edit both change to merge

--- 1.c
+++ 1.c
@@ -1,12 +1,9 @@
 //First Dummy file was written by User A
 #include <stdio.h>
 int main(){
-<<<<<<< HEAD
     print("hello world by user A");
     print("Hello world by user B1");
     print("Hello world by user B2");
-=======
     print("hello world by user A + eiei");
     print("eiei by user A")
->>>>>>> 840a95be05fa19242a81128d0aa98d384f83868e
 }
