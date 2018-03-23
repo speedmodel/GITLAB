840a95be05fa19242a81128d0aa98d384f83868e
speedmodel <punyawat16@windowslive.com>
2018-03-23 17:41:41 +0700

    add print eiei and edit print helloworld to 1.c

--- 1.c
+++ 1.c
@@ -1,5 +1,6 @@
 //First Dummy file was written by User A
 #include <stdio.h>
 int main(){
-    print("hello world by user A");
+    print("hello world by user A + eiei");
+    print("eiei by user A")
 }
